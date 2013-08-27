e5b04f2c29103bb7b1d1d8af2cea37a63118206a
J. B. Rainsberger <me@jbrains.ca>
2013-08-27 11:18:47 +0300

    We can run tests with a fully-qualified command

---
+++ test/HookupTest.php
@@ -0,0 +1,9 @@
+<?php
+#require 'vendor/autoload.php';
+
+class HookupTest extends PHPUnit_Framework_TestCase {
+  public function testIsThisThingOn() {
+    $this->assertTrue(false, "What does this message look like?");
+  }
+}
+?>

9a30812ecc89fd8a4275e7d84f3a37f5c5c14a86
J. B. Rainsberger <me@jbrains.ca>
2013-08-27 14:25:38 +0300

    Removed obsolete code

--- test/HookupTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-#require 'vendor/autoload.php';
-
-class HookupTest extends PHPUnit_Framework_TestCase {
-  public function testIsThisThingOn() {
-    $this->assertTrue(false, "What does this message look like?");
-  }
-}
-?>

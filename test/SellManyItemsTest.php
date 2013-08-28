5291446846900992b3688f782b64ba46f759cefc
J. B. Rainsberger <me@jbrains.ca>
2013-08-28 16:10:48 +0300

    Removed a test I don't intend to make pass soon.

--- test/SellManyItemsTest.php
+++ test/SellManyItemsTest.php
@@ -21,24 +21,6 @@ class SellManyItemsTest extends PHPUnit_Framework_TestCase {
 
     $this->assertEquals("Total: EUR 7.95", $display->getText());
   }
-
-  public function testManyItems() {
-    $this->markTestSkipped("When I know that I can compute the total of items in the shopping cart, then I can make this test pass.");
-
-    $display = new Display();
-    $sale = new Sale($display, new Catalog(array(
-      "12345" => "EUR 7.00",
-      "23456" => "EUR 8.00",
-      "34567" => "EUR 9.00",
-    )));
-
-    $sale->onBarcode("12345");
-    $sale->onBarcode("23456");
-    $sale->onBarcode("34567");
-    $sale->onTotal();
-
-    $this->assertEquals("Total: EUR 24.00", $display->getText());
-  }
 }
 
 ?>

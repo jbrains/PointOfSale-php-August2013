e6aba755d260f948c9c6bb096120775e34a2cefe
J. B. Rainsberger <me@jbrains.ca>
2013-08-28 16:30:36 +0300

    We are happy with our price format.

--- test/FormatPriceTest.php
+++ test/FormatPriceTest.php
@@ -21,6 +21,9 @@ class FormatPriceTest extends PHPUnit_Framework_TestCase {
     return array(
       array("EUR 0.00", 0),
       array("EUR 7.81", 781),
+      array("EUR 7.80", 780),
+      array("EUR 7.00", 700),
+      array("EUR 1237.67", 123767),
     );
   }
 

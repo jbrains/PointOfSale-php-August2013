cb918f381352225021cca3e6ceb21a0838025c59
J. B. Rainsberger <me@jbrains.ca>
2013-08-28 16:19:41 +0300

    We format €0 without hardcoding the result.

--- test/FormatPriceTest.php
+++ test/FormatPriceTest.php
@@ -1,12 +1,18 @@
 <?php
 
 class Price {
+  private $cents;
+
+  private function __constructor($cents) {
+    $this->cents = cents;
+  }
+
   public static function cents($cents) {
-    return new Price();
+    return new Price($cents);
   }
 
   public function format() {
-    return "EUR 0.00";
+    return sprintf("EUR %.2f", ($this->cents / 100.0));
   }
 }
 
@@ -14,5 +20,9 @@ class Price {
 class FormatPriceTest extends PHPUnit_Framework_TestCase {
   public function test_zero() {
     $this->assertEquals("EUR 0.00", Price::cents(0)->format());
+  }
+
+  public function xtest_no_trailing_zeroes() {
+    $this->assertEquals("EUR 7.81", Price::cents(781)->format());
   }
 }
